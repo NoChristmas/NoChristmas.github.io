f27e9411d8061b85649fe03d81f93497c51eaf4e
jeongho.park1993 <jeongho.park1993@gmail.com>
2023-05-23 17:33:40 +0900

    2023-05-23

--- Training/src/Solving/number1747.java
+++ Training/src/Solving/number1747.java
@@ -3,12 +3,21 @@ package Solving;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.BufferedReader;
-import java.util.StringTokenizer;
 
 public class number1747 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
+        int N = Integer.parseInt(br.readLine());
+        boolean flag = true;
+        int result = N+1;
+        int cnt = 0;
+        while(flag){
+            cnt = 0;
+            for(int i=1; i<=Math.sqrt(result); i++){
+                if(result%i==0) cnt++;
+                
+            }
+            result ++;
+        }
     }
 }
